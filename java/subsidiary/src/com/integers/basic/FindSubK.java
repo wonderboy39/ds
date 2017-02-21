f4c5ed8a829816066f6f9317fc94c77ef9f51729
wonderboy <wonderboy39@outlook.com>
2017-02-21 22:26:25 +0900

    정수쌍 구하기(Count all distinct pairs with difference equal to k)  이진 탐색으로 고쳐아하는 부분.  현재 2중 for문으로 인해 n^2의 시간복잡도를 보이고 있다.  이를 개선하기 위해서는 아래와 같은 조금 더 개선된 탐색기법이 필요하다.
     1) 시간 복잡도가 nlogn인 정렬기법을 이용해 배열을 정렬한다.(병합정렬 등)
      - nlogn중 그나마 쉽게 사용할수 있는 quicksort를 사용함.
    
     2) for문의 각 요소에 대해 아래와 같은 step을 거치도록 한다.
      a) arr[i] + k를 binary search로 i+1 ~ n-1까지 탐색한다.
      b) arr[i] + k를 찾으면 count 를 증가시킨다.
    
     3) count를 return한다.

--- java/subsidiary/src/com/integers/basic/FindSubK.java
+++ java/subsidiary/src/com/integers/basic/FindSubK.java
@@ -20,45 +20,86 @@ import java.util.Scanner;
  */
 
 public class FindSubK {
+	private int result, pivot, left, right;
+	public FindSubK(int [] arr){
+		pivot = 0;
+		left = 1;
+		right = arr.length-1;
+		result = 0;
+	}
+	
+	
+	public int partition(int [] arr, int start, int end){
+		result = 0;
+		pivot = start;
+		left = start+1;
+		right = end;
+		
+		while(left<right){
+			while(arr[left]<arr[pivot] && left<end) left++;
+			while(arr[right]>arr[pivot] && right>start) right--;
+			if(left<right) swap(arr, left, right);
+		}
+		
+		swap(arr, right, pivot);
+		result = right;
+		return result;
+	}
+	
+	public void quick_sort(int [] arr, int start, int end){
+		if(start<end){
+			int pos = partition(arr, start, end);
+			quick_sort(arr, start, pos-1);
+			quick_sort(arr, pos+1, end);
+		}
+	}
+	
+	public void swap(int [] arr, int left, int right){
+		int temp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = temp;
+	}
+	
+	public void displayArr(int [] arr){
+		for(int i=0; i<arr.length; i++){
+			System.out.print(String.valueOf(arr[i]) + " ");
+		}
+		System.out.println();
+	}
+	
 	public static void main(String [] args){
-		int [] arr;
+		int [] arr = null;
 		Scanner scanner = new Scanner(System.in);
-		Map<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
-		Map<Integer, ArrayList<Integer>> subMap = new HashMap<Integer, ArrayList<Integer>>();
-		Map<Integer, ArrayList<Integer>> resultMap = new HashMap<Integer, ArrayList<Integer>>();
-//			인덱스 ,  차가 k인 정수들 
+		
+		System.out.print("배열의 길이, 차 입력 : ");
 		int range = scanner.nextInt();
 		int k = scanner.nextInt();
 		
 		System.out.print("배열 요소 입력 ("+ range +"개) : ");
 		arr = new int[range];
+		FindSubK f = new FindSubK(arr);
 		for(int i=0; i<range; i++){
 			arr[i] = scanner.nextInt();
-			
-			if(resultMap.get(i).contains(arr[i])){
-				//resultMap.get(i).contains(arr[i])
-			}
-			else{
-				
-			}
-//			indexMap.put(i, arr[i]); // 그저 배열의 표현방식의 Map으로 변했을뿐 크게 달라진 표현은 아니다. 
-			ArrayList<Integer> subList = new ArrayList<Integer>();
-			subList.add(arr[i]-k);
-			subList.add(arr[i]+k);
-			subMap.put(i, subList);
-			
 		}
-
+		
+		f.quick_sort(arr, 0, arr.length-1);
+		f.displayArr(arr);
+		
 		int count = 0;
-		// arr[i=0~n]에 대해서 arr[j=i+1~ n]과의 차가 k인 정수의 갯수를 카운팅  
-		for(int i=0; i<range; i++){
-			for(int j=i+1; j<range; j++){
-				int sub = arr[i] - arr[j];
-				if(sub==k || sub== (-1) * k){
-					count++;
-				}
-			}
-		}
+		
+//		이진 탐색으로 고쳐야 하는 부분.
+//		arr[i=0~n]에 대해서 arr[j=i+1~ n]과의 차가 k인 정수의 갯수를 카운팅
+//		현재 2중 for문으로 인해 n^2의 시간복잡도를 보이고 있다. 
+//		이를 개선하기 위해서는 조금 더 개선된 탐색 기법이 필요하다.
+//		조금 더 개선된 기법인 이진탐색으로 고쳐야 한다!!!
+//		for(int i=0; i<range; i++){
+//			for(int j=i+1; j<range; j++){
+//				int sub = arr[i] - arr[j];
+//				if(sub==k || sub== (-1) * k){
+//					count++;
+//				}
+//			}
+//		}
 		
 		System.out.println("차가 " + String.valueOf(k) + "인 정수의 갯수 :: " + String.valueOf(count));
 		System.out.println("/n테스트 종료.");
